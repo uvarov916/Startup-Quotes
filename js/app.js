1693f5dc52ca3e73657e315299ffd5a5636395ec
Ivan Uvarov <tonk2012@gmail.com>
2015-02-09 13:53:59 -0500

    Improvements in data binding

--- js/app.js
+++ js/app.js
@@ -1,9 +1,6 @@
 var randomIndex = Math.floor(Math.random() * quotes.length);
 var selectedQuote = quotes[randomIndex];
 
-selectedQuote.number = '#' + selectedQuote.number;
-selectedQuote.category = '#' + selectedQuote.category;
-
 function QuoteController($scope) {
   $scope.quote = selectedQuote;
 }
