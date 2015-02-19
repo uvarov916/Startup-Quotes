aa0fd12a92caf8559277bc8bf1ed0788e7d5e8ab
Ivan Uvarov <tonk2012@gmail.com>
2015-02-18 19:25:39 -0500

    Added quotes 21-40

--- js/data.js
+++ js/data.js
@@ -118,5 +118,125 @@ var quotes = [
     text: "Your margin is MY OPPORTUNITY.",
     author: "Jeff Bezos, Amazon",
     category: "think"
+  },
+  {
+    number: "021",
+    text: "Passion never fails.",
+    author: "Paul Chen, FloNetwork, Fortiva",
+    category: "think"
+  },
+  {
+    number: "022",
+    text: "Luck is what happens when preparation meets opportunity.",
+    author: "Seneca",
+    category: "think"
+  },
+  {
+    number: "023",
+    text: "Chase the vision, not the money; the money will end up following you.",
+    author: "Tony Hsieh, Zappos.com",
+    category: "think"
+  },
+  {
+    number: "024",
+    text: "Ideas are like rabbits. You get a couple and learn how to handle them, and pretty soon you have a dozen.",
+    author: "John Steinbeck",
+    category: "think"
+  },
+  {
+    number: "025",
+    text: "Great companies don't throw money at problems, they throw ideas at problems.",
+    author: "Greg McAdoo, Sequoia Capital",
+    category: "think"
+  },
+  {
+    number: "026",
+    text: "Most of entrepreneurship is differentiating between good-sounding dumb ideas and dumb-sounding good ideas.",
+    author: "Aaron Gotwalt, CoTweet, Seesaw Decisions",
+    category: "think"
+  },
+  {
+    number: "027",
+    text: "Entrepreneur is just French for 'has ideas, does them'.",
+    author: "Alexis Ohanian, Reddit",
+    category: "think"
+  },
+  {
+    number: "028",
+    text: "Ideas are worthless until you get them out of your head to see what they can do.",
+    author: "Tanner Christensen, creative strategist",
+    category: "think"
+  },
+  {
+    number: "029",
+    text: "Most people think it's all about the idea. IT'S NOT. Everyone has ideas... The hard part is to execute on the idea.",
+    author: "Mark Cuban",
+    category: "think"
+  },
+  {
+    number: "030",
+    text: "Simple can be harder than complex. You have to work hard to get your thinking clean to make it simple.",
+    author: "Steve Jobs",
+    category: "think"
+  },
+  {
+    number: "031",
+    text: "It's hard to do a really good job on anything you don't think about in the shower.",
+    author: "Paul Graham, Y Cominator",
+    category: "think"
+  },
+  {
+    number: "032",
+    text: "The risk is not in doing something that feels risky. The risk is in not doing something that feels risky.",
+    author: "Andy Dunn, Bonobos",
+    category: "think"
+  },
+  {
+    number: "033",
+    text: "It's not about an ammount of wealth you can accumulate, it's about the impact and change you can create.",
+    author: "Neil Blumenthal, Wardy Parker",
+    category: "think"
+  },
+  {
+    number: "034",
+    text: "People can copy what you've done, but they can't copy what you're going to do.",
+    author: "Dennis Crowley, Foursquare",
+    category: "think"
+  },
+  {
+    number: "035",
+    text: "Opportunity lives at the intersection of what people need tomorrow and can be just barely built today.",
+    author: "Aaron Levie, Box",
+    category: "think"
+  },
+  {
+    number: "036",
+    text: "Embrace what you don't know, especially in the beginning because what you don't know can become your greatest asset. It ensures that you will absolutely be doing things different from everybody else.",
+    author: "Sara Blakely, Spanx",
+    category: "think"
+  },
+  {
+    number: "037",
+    text: "[The] best startups generally come from somebody needing to scratch an itch.",
+    author: "Michael Arrington, TechCrunch.",
+    category: "think"
+  },
+  {
+    number: "038",
+    text: "Start with the assumption that the best way to do something is not the way it's being done right now.",
+    author: "Aaron Levie, Box",
+    category: "think"
+  },
+  {
+    number: "039",
+    text: "Optimism should be at the top of an entrepreneur's checklist.",
+    author: "Alana Muller, Kauffman FastTrac",
+    category: "think"
+  },
+  {
+    number: "040",
+    text: "Man's mind, once stretched by a new idea, never regains its original dimensions.",
+    author: "Oliver Wendell Holmes",
+    category: "think"
   }
 ];
